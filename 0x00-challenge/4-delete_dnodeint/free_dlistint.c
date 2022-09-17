eade7b40f6f70f6fa7f0e4905735793a93915296
snipperrjay <alfredabaidoo7@gmail.com>
2022-09-17 14:57:52 +0000

    done

---
+++ 0x00-challenge/4-delete_dnodeint/free_dlistint.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - Free a list
+ *
+ * @head: A pointer to the first element of the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *node;
+
+	while (head)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
